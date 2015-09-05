a927cd6cbadf74a0bc4dee972966db926a20b239
Sujay <cheeselover724@gmail.com>
2015-09-04 23:19:13 -0500

    connecting context menu

--- WattWorkout WatchKit Extension/InterfaceController.swift
+++ WattWorkout WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,7 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -21,11 +21,21 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        println("Fucking debug shit")
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+
+
+
+    @IBAction func showNewEntryPage() {
+        println("Fucking debug shit")
+        
+//        pushControllerWithName(name: ExercisePickerView, context: nil)
     }
 
 }
